564c3e6516737e4ff050d8394f223e319df6600a
Ian McShane <mcshane.ian@gmail.com>
2019-08-26 14:41:40 +0000

    added exclusions to adhere to rules

--- easyDomainGen.py
+++ easyDomainGen.py
@@ -4,7 +4,11 @@
 # -------
 # This will generate an easy to speak domain name, in English,
 # comprised of two three letter words.
-
+# MUST not start with a vowel.
+# MUST not end with a vowel.
+# MUST not contain Q (hard to pronounce)
+# MUST not contain O or ZERO (avoid confusion)
+# MUST not contain L (avoid confusion)
 
 # Create a list of the alphabet characters - alphabet - 
 # we can use that to loop through in each iteration
@@ -12,6 +16,14 @@
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
         "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
         "v", "w", "x", "y", "z"]
+
+# Create a list of vowels
+
+vowels = ["a", "e", "i", "o", "u"]
+
+# Create a list of unwanted letters
+
+unwanted = ['q', 'o', 'l']
 
 # Create a list of strings - tlds - which hold the list 
 # of TLDs we want to use
@@ -36,8 +48,15 @@ print('The Alphabet contains ' + str(len(alphabet)) + ' letters, OK?')
 # adding the two chars to a list
 
 for letter in alphabet:
+   # check for unwanted letters
+   if letter in unwanted:
+      continue
+   if letter in vowels:
+      continue # I don't want it to start with a vowel
    currentLetter = letter
    for nextLetter in alphabet:
+      if nextLetter in unwanted:
+         continue
       twoChars.append(currentLetter + nextLetter)
 
 # SECOND PASS - create the list with three letters
@@ -45,6 +64,8 @@ for letter in alphabet:
 
 for word in twoChars:
    for letter in alphabet:
+      if letter in unwanted:
+         continue
       threeChars.append(word + letter)
 
 print('There are ' + str(len(twoChars)) + ' combinations of two chars.') # should be 676
