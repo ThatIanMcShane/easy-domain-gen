3c8e34ad716aedbaa7508de4823f24cda358452d
Ian McShane <mcshane.ian@gmail.com>
2019-08-26 22:49:30 +0100

    v0.1

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
 # easy-domain-gen
 
-I want to generate a speakable, English, domain name, comprised of two three letter words.
-The combination of words does NOT have to make any sense, pronounciation is the goal.
-This program should:
-1. Use the following rules for each three letter word:
+I wanted to generate a speakable domain name, comprised of two three letter English words. The combination of words does NOT have to make any sense, pronounciation is the goal.
 
-1.a MUST not start with a vowel.
-
-1.b MUST not end with a vowel.
-
-1.c MUST not contain Q (hard to pronounce)
-
-1.d MUST not contain O or ZERO (avoid confusion)
-
-1.e MUST not contain L (avoid confusion)
-
-2. Check if the domain exists.
-
-3. Generate a CSV of all available permutations.
+Requires pyEnchant (pip install pyenchant)
--- easyDomainGen.py
+++ easyDomainGen.py
@@ -1,14 +1,21 @@
-# EasyDomainGen.py
+#! /usr/bin/env python3
+
+# EasyDomainGen.py - @ianmcshane - August 2019
 # -------
-# @ianmcshane 2019
-# -------
-# This will generate an easy to speak domain name, in English,
-# comprised of two three letter words.
-# MUST not start with a vowel.
-# MUST not end with a vowel.
-# MUST not contain Q (hard to pronounce)
-# MUST not contain O or ZERO (avoid confusion)
-# MUST not contain L (avoid confusion)
+# REQUIRES: pyEnchant (pip install pyEnchant)
+#
+# This will return a random, easy to speak domain name, in English,
+# comprised of two three letter words.  
+# To ensure it is easy to speak, the middle letter is always a vowel.
+# We'll use pyEnchant to check the words exist in English before 
+# creating the domain names.
+
+import sys
+import random
+import enchant
+
+# Set up the dictionary for pyEnchant
+dictionary = enchant.Dict("en_US")
 
 # Create a list of the alphabet characters - alphabet - 
 # we can use that to loop through in each iteration
@@ -23,53 +30,57 @@ vowels = ["a", "e", "i", "o", "u"]
 
 # Create a list of unwanted letters
 
-unwanted = ['q', 'o', 'l']
+unwantedSounds = ['sh', 'ph']
+unwantedLetters = ['q', 'o', 'l']
 
 # Create a list of strings - tlds - which hold the list 
 # of TLDs we want to use
 
-tlds = [".com", ".net"]
-
-# Create a list of strings - twoChars - which will hold the first
-# set of characters
-twoChars = []
+tlds = [".com"]
 
 # Create a list of strings - threeChars - which will hold the final
 # three letter words
+words = []
 
-threeChars = []
+# Create the list for the generated domain names
 
-# Check the alphabet is created correctly
-print('The Alphabet contains ' + str(len(alphabet)) + ' letters, OK?')
+domains = []
 
-# FIRST PASS - create list with two letters
-# 
-# Loop through alphabet (for each item in alphabet)
-# adding the two chars to a list
-
+# Create the three letter words
 for letter in alphabet:
-   # check for unwanted letters
-   if letter in unwanted:
-      continue
-   if letter in vowels:
-      continue # I don't want it to start with a vowel
+   # if letter in vowels:
+   #   continue # I don't want it to start with a vowel
    currentLetter = letter
    for nextLetter in alphabet:
-      if nextLetter in unwanted:
-         continue
-      twoChars.append(currentLetter + nextLetter)
+      if nextLetter in vowels: # vowel in the middle
+         for lastLetter in alphabet:
+           # if lastLetter in vowels:
+            #   continue # no vowel at the end, please
+            thisWord = (currentLetter + nextLetter + lastLetter)
+            if dictionary.check(thisWord):
+               words.append(thisWord)
 
-# SECOND PASS - create the list with three letters
-# Loop through twoChars, creating a new string with each letter
+# Create all of the two-word permutations
+# Word 2 must not start with the same letter as word 1, and 
+# the domain should not appear to be one word.
 
-for word in twoChars:
-   for letter in alphabet:
-      if letter in unwanted:
-         continue
-      threeChars.append(word + letter)
+for tld in tlds:
+    for word in words:
+        for nextword in words:
+            if word == nextword:
+                continue
+            if word[2] == nextword[0]:
+                continue
+            if word[2] + nextword[0] in unwantedSounds:
+                continue
+            domains.append(word + nextword + tld)
 
-print('There are ' + str(len(twoChars)) + ' combinations of two chars.') # should be 676
+def getRandomDomain():    
+    randomNumber = random.randint(0, len(domains))
+    randomDomain = domains[randomNumber]
+    return randomDomain
 
-print('There are ' + str(len(threeChars)) + ' combinations of three chars.') # should be 17576
-
-
+if 'easyDomainGen.py' in sys.argv[0]:
+    # if script is running from a shell
+    # but with no arguments, print one random domain
+    print(getRandomDomain())
